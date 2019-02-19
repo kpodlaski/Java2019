4ef6341277f3d97d51d62799fff2e3c0ba979682
Krzysztof Podlaski <podlaski@uni.lodz.pl>
2019-02-19 11:21:47 +0100

    koniec zajec 1

--- dzienne/wtorek1/Podstawy/src/zaj1/FirstClass.java
+++ dzienne/wtorek1/Podstawy/src/zaj1/FirstClass.java
@@ -4,4 +4,24 @@ package zaj1;
  * Created by Krzysztof Podlaski on 19.02.2019.
  */
 public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("Witam w Java");
+        for (int i=0; i<10; i++){
+            System.out.println(i);
+        }
+        Person p = new Person("Adam","Małysz",40);
+        //p.name = "Adam";//new String("Adam");
+        //p.surname = "Małysz";
+        //p.age = 40;
+        p.setName("Adaś");
+        System.out.println(p.getName()+" "
+                +p.getSurname()+" lat:"+p.getAge());
+        System.out.println("osoba:" +p);
+        /*while (true){
+            Person p2 = new Person("Jane","Doe",23);
+        }
+        */
+
+    }
 }
---
+++ dzienne/wtorek1/Podstawy/src/zaj1/Person.java
@@ -0,0 +1,50 @@
+package zaj1;
+
+/**
+ * Created by Krzysztof Podlaski on 19.02.2019.
+ */
+public class Person {
+    private String name;
+    private String surname;
+    private int age;
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("DESTROY OBJECT");
+    }
+
+    @Override
+    public String toString() {
+        return this.name+" "+surname+" lat:"+age;
+    }
+}
