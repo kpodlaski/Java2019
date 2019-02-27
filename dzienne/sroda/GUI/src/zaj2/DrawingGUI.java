10f5b6d661adaa4c6e6d58620bf65c6df5b70f5c
Krzysztof Podlaski <podlaski@uni.lodz.pl>
2019-02-27 09:24:09 +0100

    drawingPanel

--- dzienne/sroda/GUI/src/zaj2/DrawingGUI.java
+++ dzienne/sroda/GUI/src/zaj2/DrawingGUI.java
@@ -3,6 +3,8 @@ package zaj2;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
 /**
  * Created by Krzysztof Podlaski on 27.02.2019.
@@ -24,8 +26,17 @@ public class DrawingGUI {
                 p.repaint();
             }
         });
-    }
+        drawingPanel.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                //super.mouseMoved(e);
+                DrawingPanel p = (DrawingPanel) drawingPanel;
+                p.moveBallTo(e.getX(),e.getY());
+                p.repaint();
 
+            }
+        });
+    }
     private void createUIComponents() {
         // TODO: place custom component creation code here
         drawingPanel = new DrawingPanel();
@@ -36,8 +47,9 @@ public class DrawingGUI {
         frame.setContentPane(new DrawingGUI().mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
-        frame.setVisible(true);
         frame.setSize(600,600);
-        frame.setLocation(300,100);
+        //frame.setLocation(300,100);
+        frame.setVisible(true);
+
     }
 }
--- dzienne/sroda/GUI/src/zaj2/DrawingPanel.java
+++ dzienne/sroda/GUI/src/zaj2/DrawingPanel.java
@@ -9,16 +9,24 @@ import java.awt.*;
 public class DrawingPanel extends JPanel {
     private int x=100;
     private int y=100;
+
+
     @Override
     public void paint(Graphics graphics) {
-        super.paint(graphics);
         Graphics2D g = (Graphics2D) graphics;
+        //g.setColor(Color.WHITE);
+        //g.fillRect(0,0,this.getWidth(),this.getHeight());
         g.setColor(Color.magenta);
-        g.fillOval(x,y,20,20);
+        g.fillOval(x-10,y-10,20,20);
     }
 
     public void moveBall(int dx, int dy) {
         x+=dx;
         y+=dy;
+    }
+
+    public void moveBallTo(int x, int y) {
+        this.x=x;
+        this.y=y;
     }
 }
