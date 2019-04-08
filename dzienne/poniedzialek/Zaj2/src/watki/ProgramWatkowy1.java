4444abee580b44b398a6da48727ddb4b574fa0d3
Krzysztof Podlaski <podlaski@uni.lodz.pl>
2019-04-08 11:45:55 +0200

    synchronizacja watkow

--- dzienne/poniedzialek/Zaj2/src/watki/ProgramWatkowy1.java
+++ dzienne/poniedzialek/Zaj2/src/watki/ProgramWatkowy1.java
@@ -7,14 +7,21 @@ import java.util.List;
  * Created by Krzysztof Podlaski on 25.03.2019.
  */
 public class ProgramWatkowy1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         List<Zliczacz> zl =new ArrayList<>();
-        zl.add(new Zliczacz("A"));
-        zl.add(new Zliczacz("B"));
+        List<Thread> threads = new ArrayList<>();
+        for (int i=0; i<1000; i++) {
+            zl.add(new Zliczacz("W" + i));
+        }
         for(Zliczacz z : zl){
             Thread th = new Thread(z);
+            threads.add(th);
             th.start();
         }
+        for(Thread t : threads){
+            t.join();
+        }
         System.out.println("KONIEC PROGRAMU");
+        System.out.println(Zliczacz.wartosc);
     }
 }
--- dzienne/poniedzialek/Zaj2/src/watki/Zliczacz.java
+++ dzienne/poniedzialek/Zaj2/src/watki/Zliczacz.java
@@ -5,16 +5,27 @@ package watki;
  */
 public class Zliczacz implements Runnable {
     private String nazwa;
+    public static int wartosc=0;
+    public static Object monitoSynchronizacji = new Object();
 
     public Zliczacz(String nazwa) {
         this.nazwa = nazwa;
     }
 
+    static synchronized void  next(){
+        wartosc++;
+    }
     public void  run(){
-        int i=1;
+        int i=0;
         while(i<100){
-            System.out.println(nazwa + " "+i);
+            System.out.println(nazwa + " "+wartosc);
             i++;
+            next();
+            /*
+            synchronized (monitoSynchronizacji) {
+                wartosc++;
+            }
+            */
         }
 
     }
