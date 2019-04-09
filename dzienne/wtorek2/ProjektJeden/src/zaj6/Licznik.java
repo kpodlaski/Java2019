4987c701f3ec7141fed4257e3264000530be1fd1
Krzysztof Podlaski <podlaski@uni.lodz.pl>
2019-04-09 12:23:06 +0200

    watki cz 1

---
+++ dzienne/wtorek2/ProjektJeden/src/zaj6/Licznik.java
@@ -0,0 +1,45 @@
+package zaj6;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Krzysztof Podlaski on 09.04.2019.
+ */
+public class Licznik  implements  Runnable{
+
+    private static int x;
+    private static Object synchMonitor =new Object();
+    private int id;
+    public Licznik(int id) {this.id=id;}
+
+    private static synchronized void next(){
+        x++;
+    }
+    @Override
+    public void run() {
+        for (int i=0; i<1000; i++){
+            /*synchronized (synchMonitor) {
+                x++;
+            }*/
+            next();
+            if (x%50==0)
+                System.out.println("w"+id+" -- "+x);
+        }
+    }
+    public static void main(String[] args) throws InterruptedException {
+        List<Thread> th = new ArrayList<>();
+        for (int i=0; i<100; i++){
+            Thread t = new Thread(new Licznik(i+1));
+            t.start();
+            th.add(t);
+        }
+
+        for (Thread t : th){
+            t.join();
+        }
+        System.out.println(x);
+        System.out.println("Koniec Programu");
+    }
+
+}
