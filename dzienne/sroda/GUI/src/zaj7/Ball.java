cdd1d6731d2b86cd53491b29c3b81dfa57eef797
Krzysztof Podlaski <podlaski@uni.lodz.pl>
2019-04-17 08:40:47 +0200

    poczta

---
+++ dzienne/sroda/GUI/src/zaj7/Ball.java
@@ -0,0 +1,35 @@
+package zaj7;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+/**
+ * Created by Krzysztof Podlaski on 17.04.2019.
+ */
+public class Ball {
+    private Image image;
+    private int x, y, r;
+
+    private Ball(BufferedImage image, int x, int y, int r) {
+        this.image=image;
+        this.x=x;
+        this.y=y;
+        this.r=r;
+    }
+
+    public static Ball create(String path,
+           int x, int y, int r) throws IOException {
+        BufferedImage image = ImageIO.read(
+        ClassLoader.getSystemResourceAsStream(path)
+        );
+        return new Ball(image,x,y,r);
+    }
+
+    public void draw(Graphics g) {
+        g.drawImage(image,x+r,y+r,
+                2*r,2*r,
+                null);
+    }
+}
