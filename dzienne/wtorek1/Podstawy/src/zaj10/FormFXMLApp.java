6e16b5ca05fe7611b2350cead5ffd1eb03149a67
Krzysztof Podlaski <podlaski@uni.lodz.pl>
2019-05-14 11:54:46 +0200

    javafx

---
+++ dzienne/wtorek1/Podstawy/src/zaj10/FormFXMLApp.java
@@ -0,0 +1,38 @@
+package zaj10;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class FormFXMLApp extends Application {
+
+    @FXML
+    private Text actiontarget;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("fxml_example.fxml"));
+
+        Scene scene = new Scene(root, 300, 275);
+
+        stage.setTitle("FXML Welcome");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void handleSubmitButtonAction(ActionEvent actionEvent) {
+        actiontarget.setText("Sign in button pressed");
+    }
+}
