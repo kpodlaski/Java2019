ebc6cd1a551f4f8161851be989e0fd99ab08d574
Krzysztof Podlaski <podlaski@uni.lodz.pl>
2019-05-18 14:37:40 +0200

    post v2

---
+++ zaoczne/semestr4/Projekt1/src/zaj4/Post.java
@@ -0,0 +1,4 @@
+package zaj4;
+
+public class Post {
+}
--- zaoczne/semestr4/Projekt1/src/zaj4/PostClient.java
+++ zaoczne/semestr4/Projekt1/src/zaj4/PostClient.java
@@ -1,5 +1,22 @@
 package zaj4;
 
-public class PostClient {
+public class PostClient implements Runnable {
     public int  id;
+    Post post;
+
+    public PostClient(int id, Post post){
+        this.id=id;
+        this.post=post;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Klient "+id+" wchodzi na Pocztę");
+        post.serve(this);
+        System.out.println("Klient "+id+" idzie do domu");
+    }
+
+    public void task(PostClerk clerk){
+        //TODO and use
+    }
 }
